355e01898082e05ca406f9d0a50d89233778eb31
raj singh <rs457036@gmail.com>
2021-01-02 06:54:37 +0530

    fixed imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'dart:io';
-
-import 'package:boiler_plate/app/core/connection/network_availability.dart';
-import 'package:boiler_plate/app/core/core.dart';
-import 'package:boiler_plate/app/global/connectivity/connectivity_handler.dart';
-import 'package:boiler_plate/app/utils/function/logger.dart';
-import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'app/core/core.dart';
 import 'app/routes/app_pages.dart';
 import 'app/routes/app_route_observer.dart';
 import 'app/utils/function/di.dart';
