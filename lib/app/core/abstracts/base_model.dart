3f7acfedc92dfd7c78e02b35881b243384b91f47
raj singh <rs457036@gmail.com>
2021-01-07 05:16:54 +0530

    _

--- lib/app/core/abstracts/base_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class BaseModel extends Equatable {
-  String toJson();
-  Map<String, dynamic> toMap();
-}
--- lib/app/core/core.dart
+++ lib/app/core/core.dart
@@ -3,3 +3,4 @@ export 'exceptions/exceptions.dart';
 export 'failures/failures.dart';
 export 'exception_failure_mapper.dart';
 export 'logger/logger.dart';
+export 'abstracts/abstracts.dart';
